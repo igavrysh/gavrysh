09a3d2fd176aa376feca6143a653f15cfb0ef918
Student 104 <student104@Antons-iMac-2.local>
2016-04-04 20:11:45 +0300

    first commit for mln start up

---
+++ SuperCProject/SuperCProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SuperCProject
+//
+//  Created by Student 104 on 4/4/16.
+//  Copyright (c) 2016 1milliondollarsstartup. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, 1mln dollars startup!\n");
+    return 0;
+}
