af3253f3e35c964ca8915c0acd3fa27fa47c00e1
Ievgen Gavrysh <igavrysh@gmail.com>
2016-04-11 20:29:32 +0300

    add unionized structure

--- SuperCProject/SuperCProject/Sources/IDPStructures/IDPStructures.h
+++ SuperCProject/SuperCProject/Sources/IDPStructures/IDPStructures.h
@@ -6,19 +6,9 @@
 //  Copyright © 2016 Ievgen Gavrysh. All rights reserved.
 //
 
-/*
-Задачи:
-1. Создать структуру, данных, хранящую в себе расположенные в случайном порядке 6 bool, 1X float, 1X int, 1X long long, 3X short, 1X double, 1 Xуказатель на строку.
-Требования:
-- создать тестовый метод, который бы с помощью offsetof выводил расположение каждого из элементов;
-- создать тестовый метод, который бы выводил размер всей структуры;
-- перекомпоновать структуру так, чтобы она занимала наименьшее количество места;
-- поместить bool значения в битовое поле и создать union;
-2. Создать метод для вывода битов числа в консоль.
-*/
-
 #ifndef IDPStructures_h
 #define IDPStructures_h
+
 #include <stdbool.h>
 
 typedef struct IDPStartUp IDPStartUp;
@@ -57,6 +47,32 @@ struct IDPStartUpCompressed {
     bool _isMultipleFounders;
     bool _isFounderMajorStakeholder;
     bool _isCreditBorrowed;
+};
+
+typedef struct IDPStartUpUnionized IDPStartUpUnionized;
+
+struct IDPStartUpUnionized {
+    char *_startUpName;
+    //
+    float _revenueEarned;
+    int _suppliersNum;
+    //
+    double _profitEarned;
+    //
+    long long _usersCovered;
+    //
+    union {
+        struct {
+            bool _isAngelInvestments;
+            bool _isVentureFondInverstments;
+            bool _isGovernmetnInverstments;
+            bool _isMultipleFounders;
+            bool _isFounderMajorStakeholder;
+            bool _isCreditBorrowed;
+        } flags;
+        char bitFields;
+    };
+    short _employeesNum;
 };
 
 #endif /* IDPStructures_h */
