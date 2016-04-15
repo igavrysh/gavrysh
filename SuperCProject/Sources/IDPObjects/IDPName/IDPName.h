a150a64ec4e93d867c2d5db0d9535989edd7d36d
Ievgen Gavrysh <igavrysh@gmail.com>
2016-04-15 20:21:35 +0300

    add IDPName super as IDP object

--- SuperCProject/Sources/IDPObjects/IDPName/IDPName.h
+++ SuperCProject/Sources/IDPObjects/IDPName/IDPName.h
@@ -9,9 +9,14 @@
 #ifndef __SuperCProject__IDPName__
 #define __SuperCProject__IDPName__
 
+#include "IDPObject.h"
+
 typedef struct IDPName IDPName;
 
 struct IDPName {
+    IDPObject *_super;
+    
+    
     
 };
 
