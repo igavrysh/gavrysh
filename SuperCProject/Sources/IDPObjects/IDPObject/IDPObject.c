a054345ea937def504cee0b0e5e8fe847c838e62
Ievgen Gavrysh <igavrysh@gmail.com>
2016-04-15 20:17:47 +0300

    add IDPObject

---
+++ SuperCProject/Sources/IDPObjects/IDPObject/IDPObject.c
@@ -0,0 +1,51 @@
+//
+//  IDPObject.c
+//  IDPSeptemberGroupCode
+//
+//  Created by Ievgen on 4/13/16.
+//  Copyright © 2016 Ievgen Gavrysh. All rights reserved.
+//
+
+#include <assert.h>
+
+#include "IDPObject.h"
+
+void *__IDPObjectCreate(size_t objectSize, IDPObjectDeallocatorCallback deallocateCallback) {
+    assert (0 != objectSize);
+    
+    IDPObject *object = calloc(1, objectSize);
+    
+    assert(NULL != object);
+    
+    object->_referenceCount = 1;
+    object->_deallocator = deallocateCallback;
+    
+    return object;
+}
+
+void *IDPObjectRetain(void *object) {
+    if (NULL != object) {
+        ((IDPObject *)object)->_referenceCount++;
+    }
+    
+    return object;
+}
+
+void IDPObjectRelease(void *object) {
+    if (NULL != object) {
+        uint64_t count = ((IDPObject *)object)->_referenceCount - 1;
+        ((IDPObject *)object)->_referenceCount = count;
+        
+        if (0 == count) {
+            ((IDPObject *)object)->_deallocator(object);
+        }
+    }
+}
+
+uint64_t IDPObjectGerReferenceCount(void *object) {
+    return object ? ((IDPObject *)object)->_referenceCount : 0;
+}
+
+void __IDPObjectDeallocate(void *object) {
+    free(object);
+}
---
+++ SuperCProject/Sources/IDPObjects/IDPObject/IDPObject.h
@@ -0,0 +1,39 @@
+//
+//  IDPObject.h
+//  IDPSeptemberGroupCode
+//
+//  Created by Ievgen on 4/13/16.
+//  Copyright © 2016 Ievgen Gavrysh. All rights reserved.
+//
+
+#ifndef IDPObject_h
+#define IDPObject_h
+
+#include <stdlib.h>
+
+typedef void (*IDPObjectDeallocatorCallback)(void *);
+
+typedef struct {
+    uint64_t _referenceCount;
+    IDPObjectDeallocatorCallback _deallocator;
+} IDPObject;
+
+extern
+void *__IDPObjectCreate(size_t objectSize, IDPObjectDeallocatorCallback deallocateCallback);
+
+#define IDPObjectCreateOfType(type) \
+__IDPObjectCreate(sizeof(type), (IDPObjectDeallocatorCallback)__##type##Deallocate)
+
+extern
+void *IDPObjectRetain(void *object);
+
+extern
+void IDPObjectRelease(void *object);
+
+extern
+void __IDPObjectDeallocate(void *object);
+
+extern
+uint64_t IDPObjectGerReferenceCount(void *object);
+
+#endif /* IDPObject_h */
