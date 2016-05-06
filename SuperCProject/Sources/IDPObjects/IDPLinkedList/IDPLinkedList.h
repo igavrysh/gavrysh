bf5e068b8f201a1c5dec7de5a313b66d9fbf763f
Ievgen Gavrysh <igavrysh@gmail.com>
2016-05-06 19:40:28 +0300

    add IPDLinkedList initial structure

--- SuperCProject/Sources/IDPObjects/IDPLinkedList/IDPLinkedList.h
+++ SuperCProject/Sources/IDPObjects/IDPLinkedList/IDPLinkedList.h
@@ -9,6 +9,18 @@
 #ifndef IDPLinkedList_h
 #define IDPLinkedList_h
 
-#include <stdio.h>
+#include "IDPObject.h"
+#include "IDPLinkedListNode.h"
+
+typedef struct IDPLinkedList IDPLinkedList;
+
+struct IDPLinkedList {
+    IDPObject _super;
+    
+    IDPLinkedListNode *_head;
+    uint64_t _count;
+};
+
+
 
 #endif /* IDPLinkedList_h */
