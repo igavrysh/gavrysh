10890c432218b3307a37f8161af614253b0c9d01
Ievgen Gavrysh <igavrysh@gmail.com>
2016-05-29 17:10:38 +0300

    add obj c project into project structure

---
+++ SuperCProject/SuperObjC/SuperObjCProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SuperObjCProject
+//
+//  Created by Ievgen on 5/29/16.
+//  Copyright © 2016 Ievgen Gavrysh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
